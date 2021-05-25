885f6101b6c77d141abaa8c4b8428ef507ea1aa8
Seth Duncan <sethduncan@gmail.com>
2021-05-25 15:56:16 -0500

    move command handling to back end

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,104 +1,27 @@
-var codeURL = 'https://www.github.com/DJSethDuncan'
-var musicURL = 'https://www.soundcloud.com/sethduncanmusic'
-var filePermissionText = '---x------ root root'
-
 var primaryColor = 'lightblue'
-
-var cv = `
-Seth Duncan
-sethduncan@gmail.com
-
-Engineering Manager
-Marchex
-February 2015 - Present
-`
-
-var helpText = `
-Commands:
-
-[[gb;${primaryColor};black]ls]              List files
-[[gb;${primaryColor};black]help]            You already know what this does
-`
-
-var fileList = {
-    code: {
-        description: 'Secret software DO NOT OPEN'
-    },
-    music: {
-        description: 'My sick tunes'
-    }
-}
-
-var hiddenFileList = {
-    '.cv': {
-        descriptiont: 'My resume'
-    }
-}
-
-var codeCommands = {
-    "code": function () {
-        this.echo('Launching code... \n')
-        window.open(codeURL)
-    }
-}
-
-var musicCommands = {
-    "music": function () {
-        this.echo('Launching music... \n')
-        window.open(musicURL)
-    }
-}
-
-var terminalCommands = {
-    "ls": function (param) {
-        var fileOutput = '\n'
-        if (param == "-la") {
-            for (hiddenFile in hiddenFileList) {
-                fileOutput += filePermissionText + ` May 24 00:41 [[gb;${primaryColor};black]` + hiddenFile + `]\n`
-            }
-            for (file in fileList) {
-                fileOutput += filePermissionText + ` May 24 00:41 [[gb;${primaryColor};black]` + file + `]\n`
-            }
-        } else if (param == "-l") {
-            for (file in fileList) {
-                fileOutput += filePermissionText + ` May 24 00:41 [[gb;${primaryColor};black]` + file + `]\n`
-            }
-        } else {
-            for (file in fileList) {
-                fileOutput += `[[gb;${primaryColor};black]` + file + `] `
-            }
-            fileOutput += `\n`
-        }    
-        this.echo(fileOutput)
-    },
-    '.cv': function () {
-        this.echo(cv + '\n\n')
-    },
-    help: function () {
-        this.echo(helpText)
-    }
-}
-
 var terminalParams = {
     greetings: `Welcome to [[gb;${primaryColor};black]sethduncan.com] \n \n`,
     name: 'sethduncan.com',
     prompt: `[[gb;pink;black]$][[gb;purple;black]>] `,
-    checkArity: false
+    checkArity: false,
 }
 
-Object.assign(terminalCommands, codeCommands)
-Object.assign(terminalCommands, musicCommands)
-
-$(function ($, undefined) {
-    // TODO make this hit the API with the command instead
-    // $('#terminal').terminal(function (command) {
-    //     $.ajax({...})
-    // }, {
-    //     greetings: 'Welcome to sethduncan.com \n \n',
-    //     name: 'sethduncan.com',
-    //     prompt: 'SD)> '
-    // })
-
-    $('#terminal').terminal(terminalCommands, terminalParams)
-
-})
+$(function() {
+    $('#terminal').terminal(function(command, term) {
+        term.pause();
+        $.post('/cmd', {command: command}).then(function(response) {
+            var consoleText = '';
+            if (response.status == 'ok') {
+                if (response.text !== '') {
+                    consoleText = response.text;
+                }
+                if (response.url !== '') {
+                    window.open(response.url)
+                }
+            } else {
+                consoleText = 'Unknown command';
+            }
+            term.echo(`\n${consoleText}\n`).resume();
+        });
+    }, terminalParams);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,107 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+var codeURL = 'https://www.github.com/DJSethDuncan'
+var musicURL = 'https://www.soundcloud.com/sethduncanmusic'
+var filePermissionText = '---x------ root root'
+var primaryColor = 'lightblue'
+
+var cv = `
+Seth Duncan
+sethduncan@gmail.com
+
+Engineering Manager
+Marchex
+February 2015 - Present
+`
+
+var helpText = `
+Commands:
+
+[[gb;${primaryColor};black]ls]              List files
+[[gb;${primaryColor};black]help]            You already know what this does
+`
+
+var fileList = {
+    code: {
+        description: 'Secret software DO NOT OPEN'
+    },
+    music: {
+        description: 'My sick tunes'
+    }
+}
+
+var hiddenFileList = {
+    '.cv': {
+        descriptiont: 'My resume'
+    }
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.post('/cmd', function(req, res, next) {
+  try {
+    var responseData = processCommand(req.body.command)
+    res.json(responseData)
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+// functions
+function processCommand (command) {
+
+  command = command.trim()
+  
+  var commandResponse = {
+    'status': 'ok',
+    'data': [],
+    'text': '',
+    'url': ''
+  }
+
+  switch (command) {
+    case 'help':
+      commandResponse.text = helpText
+      break
+    case 'code':
+      commandResponse.text = 'Loading code...'
+      commandResponse.url = codeURL
+      break
+    case 'music':
+      commandResponse.text = 'Loading music...'
+      commandResponse.url = musicURL
+      break
+    case '.cv':
+      commandResponse.text = cv
+      break;
+    case 'ls':
+      for (file in fileList) {
+          commandResponse.text += `[[gb;${primaryColor};black]` + file + `] `
+      }
+      break;
+    case 'ls -l':
+      for (file in fileList) {
+          commandResponse.text += filePermissionText + ` May 24 00:41 [[gb;${primaryColor};black]` + file + `]\n`
+      }
+    case 'ls -la':
+      for (hiddenFile in hiddenFileList) {
+                commandResponse.text += filePermissionText + ` May 24 00:41 [[gb;${primaryColor};black]` + hiddenFile + `]\n`
+            }
+            for (file in fileList) {
+                commandResponse.text += filePermissionText + ` May 24 00:41 [[gb;${primaryColor};black]` + file + `]\n`
+            }
+      break;
+    default:
+      commandResponse.text = 'Unknown command "' + command + '"'
+      break
+  }
+
+  return commandResponse
+}
+
+module.exports = router
